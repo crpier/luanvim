6d26e6fc11fadd7e7a9862f764c3fe565a8bf97d
tiannaru <piersinaru.cristian@gmail.com>
2021-06-22 18:01:57 +0300

    initial commit

---
+++ lua/keymappings.lua
@@ -0,0 +1,28 @@
+-- Leader key and leader keymaps
+vim.api.nvim_set_keymap('n', '<Space>', '<NOP>', { noremap = true, silent = true })
+vim.g.mapleader = ' '
+vim.api.nvim_set_keymap('n', '<Leader>h', ':set hlsearch!<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Leader>e', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
+-- Window movement with arrows
+vim.api.nvim_set_keymap('n', '<Left>', '<C-w>h', {silent = true})
+vim.api.nvim_set_keymap('n', '<Down>', '<C-w>j', {silent = true})
+vim.api.nvim_set_keymap('n', '<Up>', '<C-w>k', {silent = true})
+vim.api.nvim_set_keymap('n', '<Right>', '<C-w>l', {silent = true})
+-- resize with ctrl+arrows -- haha mac users cry
+vim.api.nvim_set_keymap('n', '<C-Up>', ':resize -2<CR>', {silent = true})
+vim.api.nvim_set_keymap('n', '<C-Down>', ':resize +2<CR>', {silent = true})
+vim.api.nvim_set_keymap('n', '<C-Left>', ':vertical resize -2<CR>', {silent = true})
+vim.api.nvim_set_keymap('n', '<C-Right>', ':vertical resize +2<CR>', {silent = true})
+-- Exit insert mode easily
+vim.api.nvim_set_keymap('i', 'jk', '<ESC>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('i', 'jw', '<ESC>:w<CR>', {noremap = true, silent = true})
+-- Create newlines
+vim.api.nvim_set_keymap('n', 'go', 'o<Esc>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'gO', 'O<Esc>', { noremap = true, silent = true })
+-- I don't use EX mode, and I keep pressing it by mistake when I want to quit
+vim.api.nvim_set_keymap('n', 'Q', 'ZQ', { noremap = true, silent = true })
+-- Use tab to switch buffers -- TODO maybe remove
+vim.api.nvim_set_keymap('n', '<TAB>', ':bnext<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<S-TAB>', ':bprev<CR>', { noremap = true, silent = true })
+-- Exit term easy
+vim.api.nvim_set_keymap('t', '<C-\\>', '<C-\\><C-n>', { noremap = true, silent = true })
---
+++ lua/plugins.lua
@@ -0,0 +1,18 @@
+local execute = vim.api.nvim_command
+local fn = vim.fn
+
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+
+if fn.empty(fn.glob(install_path)) > 0 then
+  fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
+  execute 'packadd packer.nvim'
+end
+
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+return require('packer').startup(function()
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+  use 'morhetz/gruvbox'
+  use 'kyazdani42/nvim-tree.lua'
+end)
